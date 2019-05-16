47cd40debe1dca54bf7619b945f4a7200023830d
terryllowery <terry@lowerytech.net>
2019-05-16 18:45:35 -0500

    wip

--- src/net/lowerytech/app/MusicApp.java
+++ src/net/lowerytech/app/MusicApp.java
@@ -1,9 +1,11 @@
 package net.lowerytech.app;
 
+import net.lowerytech.console.Console;
 import net.lowerytech.db.MusicDB;
 import net.lowerytech.models.Artist;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MusicApp {
 
@@ -11,9 +13,23 @@ public class MusicApp {
         MusicDB musicDB = new MusicDB();
         List<Artist> artists = musicDB.getAll();
 
-        for(Artist a : artists) {
-            System.out.println(a.toString());
-        }
+        Console.printHeader("Artists");
+        List<String> artistNames = artists.stream()
+                .map(a -> a.getName())
+                .distinct()
+                .collect(Collectors.toList());
+        artistNames.stream()
+                .forEach(System.out::println);
+
+        System.out.println();
+        Console.printHeader("Albums");
+        List<String> albumNames = artists.stream()
+                .map(a -> a.getAlbum().getName())
+                .collect(Collectors.toList());
+        albumNames.stream()
+                .forEach(System.out::println);
+
+
 
 
     }
---
+++ src/net/lowerytech/console/Console.java
@@ -0,0 +1,9 @@
+package net.lowerytech.console;
+
+public class Console {
+
+    public static void printHeader(String prompt) {
+        System.out.println(prompt);
+        System.out.println("---------");
+    }
+}
